e42224be953f707dab2a4c07d0ca704b28c01e00
Shavon Harris <Shavonharris9114@gmail.com>
2022-04-24 16:08:02 -0700

    added router

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import ContactCard from './ContactCard'
+import React from "react";
+import { Link } from "react-router-dom";
+import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
-    console.log("props", props)
+  console.log(props);
 
-    const renderContactList = props.contacts.map((contact) => {
-        return (
-            <ContactCard contact={contact}
-            />
-        );
-    })
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHander={deleteContactHandler}
+        key={contact.id}
+      />
+    );
+  });
   return (
-    <div className="ui celled list" key={props.contacts.email} >
-      {renderContactList}
+    <div className="main">
+      <h2>
+        Contact List
+        <Link to="/add">
+          <button className="ui button blue right">Add Contact</button>
+        </Link>
+      </h2>
+      <div className="ui celled list">{renderContactList}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactList
+export default ContactList;
